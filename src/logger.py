01c1c65d9f34ac6ee2510f9eb889b4f8025a879e
ML Developer <dev@mlproject.local>
2026-06-06 00:33:44 +0200

    logging and exception

--- src/logger.py
+++ src/logger.py
@@ -10,4 +10,6 @@ LOG_FIEL_PATH=os.path.join(logs_path,LOG_FIEL)
 logging.basicConfig(
     filename=LOG_FIEL_PATH,format="[%(asctime)s]%(lineno)d %(name)s - %(levelname)s -%(message)s," \
     "level=logging.INFO, ")
+
+
  
